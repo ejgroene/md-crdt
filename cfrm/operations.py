3aa7eb437ac50b62d1167e0cff3dfa1e143544ea
Erik <erik@seecr.nl>
2023-09-07 07:29:36 +0000

    Simplyfied operations to add/delete triple

--- cfrm/operations.py
+++ cfrm/operations.py
@@ -2,79 +2,99 @@ from autotest import get_tester
 test = get_tester(__name__)
 
 import uuid
+import datetime
 
 
-class Operation(dict):
+class Operation:
 
-    @staticmethod
-    def create(identifier):
-        return Operation(type='create', identifier=identifier)
+    __slots__ = ['id', 'timestamp', 'base']
 
-    @staticmethod
-    def insert(base, predicate, value):
-        return Operation(type='insert', base=base, predicate=predicate, value=value)
-
-    def __init__(self, id=None, **kw):
-        id = id or uuid.uuid1()
-        super().__init__(id=id, **kw)
+    def __init__(self, id, base):
+        self.timestamp = datetime.datetime.now()
+        self.base = base
+        self.id = id or uuid.uuid1()
 
 
-def materialize(*ops):
-    "Create a mutable object from the given operations"
-    ob = None
-    for op in ops:
-        if op['type'] == 'create':
-            ob = {'identifier': op['identifier']}
-        elif op['type'] == 'insert':
-            ob[op['predicate']] = op['value']
+class Insert(Operation):
+
+    __slots__ = ['key', 'value']
+
+    def __init__(self, id, base, key, value):
+        super().__init__(id, base)
+        self.key = key
+        self.value = value
+
+    def apply(self, obj):
+        if node := obj.get_node(self):
+            pass
         else:
-            raise Exception
-    return ob
+            node = obj.create_root(self.id)
+        obj.add_edge(node, self.key, self.value)
 
 
-op0 = Operation.create(identifier='id_0')
-test.eq('create', op0['type'])
-test.eq('id_0', op0['identifier'])
-test.isinstance(op0['id'], uuid.UUID)
+class Delete(Operation):
+    def apply(self, obj):
+        pass
+
+
+class Object(list):
+
+    __slots__ = ['_index']
+
+    def __init__(self):
+        self._index = {}
+
+    def get_node(self, op):
+        return self._index.get(op.id)
+
+    def create_root(self, id):
+        node = {'@id': id}
+        self.append(node)
+        self._index[node['@id']] = node
+        return node
+
+    def add_edge(self, node, key, value):
+        node.setdefault(key, []).append(value)
+        if isinstance(value, dict) and '@id' in value:
+            self._index[value['@id']] = value
+        
+    def apply_all(self, *ops):
+        for op in ops:
+            op.apply(self)
+
 
 @test
-def create_op():
-    rep0 = materialize(op0)
-    test.eq('id_0', rep0.pop('identifier'))
-    test.comp.contains(rep0, 'id')
-    test.eq({}, rep0)
-
-op1 = Operation.insert(base=op0['id'], predicate='name', value='Whepagong')
-test.eq('insert', op1['type'])
-test.eq(op0['id'], op1['base'])
-test.eq('name', op1['predicate'])
-test.eq('Whepagong', op1['value'])
-
+def create_object():
+    o = Object()
+    o.apply_all(Insert(base=None, id='root0', key='A', value=42))
+    test.eq([{'@id': 'root0', 'A': [42]}], o)
+    o.apply_all(Insert(base=None, id='root0', key='B', value='smoo'))
+    test.eq([{'@id': 'root0', 'A': [42], 'B': ['smoo']}], o)
+    o.apply_all(Insert(base=None, id='root0', key='A', value='fortytwo'))
+    test.eq([{'@id': 'root0', 'A': [42, 'fortytwo'], 'B': ['smoo']}], o)
 
 @test
-def add_property():
-    rep0 = materialize(op0, op1)
-    test.eq('id_0', rep0.pop('identifier'))
-    test.eq('Whepagong', rep0.pop('name'))
-    test.eq({}, rep0)
-
-
-@test
-def insert_property():
-    op2 = Operation.insert(base=op1['id'], predicate='town', value='Plywaid')
-    rep0 = materialize(op0, op1, op2)
-    test.eq({
-       'identifier':
-         'id_0',
-       'name':
-         'Whepagong',
-       'town':
-         'Plywaid',
-        }, rep0, diff=test.diff2)
+def create_sub_nodes():
+    o = Object()
+    o1 = Insert(base=None, id='root0', key='S', value={'@id': 'sub0'})
+    o2 = Insert(base=o1, id='sub0', key='Z', value={'@id': 'sub1'})
+    o.apply_all(o1, o2)
+    test.eq([{'@id': 'root0', 'S': [{'@id': 'sub0', 'Z': [{'@id': 'sub1'}]}]}], o, diff=test.diff2)
+    o3 = Insert(base=o1, id='sub1', key='B', value=42)
+    o.apply_all(o3)
+    test.eq([{'@id': 'root0', 'S': [{'@id': 'sub0', 'Z': [{'@id': 'sub1', 'B': [42]}]}]}], o, diff=test.diff2)
 
 @test
 def concurrent_edit():
-    op1_1 = Operation.insert(base=op1['id'], predicate='name', value='Klozask')
-    rep0 = materialize(op0, op1, op1_1)
-    test.eq({}, rep0, diff=test.diff2)
+    """ These actually do not exists... every triple is unique. What is possible though is 
+        a triple being deleted concurrently """
+
+@test
+def delete_value():
+    o = Object()
+    o.apply_all(Insert(base=None, id='root0', key='A', value=42))
+    test.eq([{'@id': 'root0', 'A': [42]}], o)
+    o.apply_all(Delete(id='del0', base='root0'))
+
+
 
