14e146d76a436a888ea146f21d7079853d984c90
Erik <erik@seecr.nl>
2023-10-04 10:11:50 +0000

    sorted out most bundling/js/mocha/shoelace woes

--- cfrm/static/editor.js
+++ cfrm/static/editor.js
@@ -1,112 +1,149 @@
 
-const {assert, expect, should} = chai
+const {assert, expect, should} = chai       // chai is globally imported earlier
+import {LitElement, html, css} from "lit"
 
-import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js'
 
 // import explicitly, because the autoloader does not work in this context
-import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.9.0/cdn/components/card/card.js'
-import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.9.0/cdn/components/rating/rating.js'
-import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.9.0/cdn/components/button/button.js'
-import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.9.0/cdn/components/input/input.js'
+import 'shoelace/components/card/card.js'
+import 'shoelace/components/button/button.js'
+import 'shoelace/components/icon/icon.js'
+import 'shoelace/components/input/input.js'
+import 'shoelace/components/tree/tree.js'
+import 'shoelace/components/tree-item/tree-item.js'
 
 
 export class Editor extends LitElement {
 
-  static get properties() {
-    return {
-      version: { type: String, reflect: true },
-      triples: { state: true },
-    }
+  // declare pokeable properties that cause automagic rerendering
+  static properties = {
+    I_am_here_to_remember: { type: String}
   }
 
-  static styles = css`
-      .data {
-        border-bottom: 1px solid lightgray;
-        border-left: 1px solid lightgray;
-        padding: 0.5em;
+  static styles = [
+    css`
+      sl-button {
+        padding-left: 0.5em;
       }
-    `
+      sl-card::part(header) {
+        font-family: var(--sl-font-sans);
+      }
+      sl-input::part(base) {
+        border: none;
+        box-shadow: none;
+        border-radius: 0;
+        height: 100%;
+      }
+      sl-input::part(input) {
+        padding-left: 0em;
+      }
+      sl-input::part(input):focus {  
+        border-bottom: 2px dotted lightgray
+      }
+    `]
 
   constructor() {
     super();
     this.version = '0.0.1';
     this.triples = [
-      {subject: "s1", predicate: "p1", object: "o1"},
+      {subject: "urn:john", predicate: "Name", object: "John Happy"},
+      {subject: "urn:john", predicate: "Address", object: "urn:address-john"},
+      {subject: "urn:address-john", predicate: "Street", object: "Happy Alley 16"}
     ]
+    this.root = "urn:john"
     this.new_triple = {subject: "", predicate: "", object: ""}
   }
 
-  _submit(event) {
-    console.log("submit", event)
-    const form = document.querySelector('form')
-    console.log("form", form)
-    const data = new FormData(form)
-    console.log("data", data)
-  }
 
-  delete(i) {
+  delete(t) {
+    const i = this.triples.indexOf(t)
     this.triples.splice(i, 1)
     this.requestUpdate()
     // publish a delete operation to the network
   }
 
-  add_triple(event) {
-    console.log("add_triple")
+
+  commit_triple() {
     this.triples.push(this.new_triple)
     this.new_triple = {subject: "", predicate: "", object: ""}
     this.requestUpdate()
+    // publish a create operation to the network
   }
 
-  set_new_subject(event) {
-    this.new_triple.subject = event.target.value
+  new_subject(subject) {
+    this.new_triple.subject = subject
+    this.requestUpdate()
   }
     
-  set_new_predicate(event) {
+  new_predicate(event) {
     this.new_triple.predicate = event.target.value
   }
 
-  set_new_object(event) {
+  new_object(event) {
     this.new_triple.object = event.target.value
   }
 
-  render() {
+  render_triple_edit() {
     return html`
-      <sl-card class="card-header card-footer">
-        <div slot="header">
-          Decentralized On/Offline Collaborative Conflict Free Structured Data Editor ${this.version}.
-        </div>
-        <table>
-          <tr>
-            <th>Subject</th>
-            <th>Predicate</th>
-            <th>Object</th>
-            <th></th>
-          </tr>
-          ${this.triples.map( (triple, i)=> html`
-            <tr>
-              <td class="data">${triple.subject}</td>
-              <td class="data">${triple.predicate}</td>
-              <td class="data">${triple.object}</td>
-              <td><sl-button variant="danger" @click="${e => this.delete(i)}">Delete</sl-button></td>
-            </tr>
-          `)}
-          <form id="yourform">
-          <tr>
-            <td><sl-input @input=${this.set_new_subject}></sl-input></td>
-            <td><sl-input @input=${this.set_new_predicate}></sl-input></td>
-            <td><sl-input @input=${this.set_new_object}></sl-input></td>
-            <td><sl-button type="submit" variant="primary" @click="${this.add_triple}">Add</sl-button></td>
-          </tr>
-          </form>
-        </table>
-        <div slot="footer">
-        </div>
-      </sl-card>
-    `
+      <sl-tree-item expanded>
+        <sl-input name="predicate" placeholder="Predicate" @input="${e => this.new_predicate(e)}"></sl-input>
+        <sl-tree-item>
+          <sl-input name="object" placeholder="Value" @input="${e => this.new_object(e)}"></sl-input>
+          <sl-icon-button name="save" @click="${e => this.commit_triple()}"></sl-icon-button>
+        </sl-tree-item>
+      </sl-tree-item>`
+  }
+    
+  render_one_object(subject) {
+    let triples = this.triples.filter(t => t.subject == subject)
+    if (triples.length == 0) {
+      if (this.new_triple.subject != subject) {
+        return html`
+          <sl-tree-item>
+            no properties (${subject})
+            <sl-icon-button name="plus" @click="${e => this.new_subject(subject)}"></sl-icon-button>
+          </sl-tree-item>`
+      } else {
+        return this.render_triple_edit()
+      }
+    } else {
+    return html`
+      ${triples.map((t, i, all) => {
+        return html`
+          <sl-tree-item expanded>
+            ${t.predicate}
+            ${t.object.startsWith("urn:")
+              ? this.render_one_object(t.object)
+              : html`<sl-tree-item>${t.object}
+                       <sl-icon-button name="trash" @click="${e => this.delete(t)}"></sl-icon-button>
+                     </sl-tree-item>`
+            }
+          </sl-tree-item>
+          ${i == all.length - 1
+            ? html`<sl-icon-button name="plus" @click="${e => this.new_subject(subject)}"></sl-icon-button>`
+            : html``  
+          }
+          ${t.subject == this.new_triple.subject && i == all.length - 1
+            ? this.render_triple_edit()
+            : html``
+          }
+        `
+      })}
+    `}
   }
 
-  my_logic() {
-    return "I am logic, or what?"
+  render() {
+    const name_triple = this.triples.find(t => t.predicate == "Name" && t.subject == this.root)
+    const title = name_triple ? name_triple.object : this.root
+    return html`
+      <sl-card class="card-header">
+        <div slot="header">
+          <p>${title} ${this.version}</p>
+        </div>
+        <sl-tree>
+          ${this.render_one_object(this.root)}
+        </sl-tree>
+      </sl-card>
+    `
   }
 }
 
@@ -115,7 +152,7 @@ customElements.define('editor-element', Editor);
 
 
 function with_host(tests) {
-    // components need to be attached to the DOM to render
+    // test helper; components need to be attached to the DOM to render
     const host = document.createElement('div')
     document.body.appendChild(host)
     try {
@@ -130,10 +167,10 @@ describe("Editor", () => {
   it('Create Editor Object', () => {
     const editor = new Editor();
     expect(editor).not.to.be.null
-    expect(editor.my_logic()).to.equal("I am logic, or what?")
     expect(editor.version).to.equal('0.0.1')
     const {strings, values} = editor.render()
-    expect(values[0]).to.equal(editor.version)
+    expect(values[0]).to.equal("John Happy")
+    expect(values[1]).to.equal(editor.version)
     }) 
   
   it('Create Editor Element', async () => {
--- cfrm/static/libs.mjs
+++ cfrm/static/libs.mjs
@@ -1,7 +1,10 @@
 
-
+// some libraries taken from npm and converted to es6 modules using webpack
+// the conversion is done by the webpack.config.js file
+// the libraries are then bundled into a single file libs-pack.mjs
 export {default as rdf} from "rdf-ext"
 export {default as lodash} from "lodash"
 export * as combinatorics from "js-combinatorics"
 export {default as rdf_formats} from '@rdfjs/formats-common'
 export { Readable} from 'readable-stream'
+
